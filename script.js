112f19cfe5ba558ca0082562c8ad22a5a56069db
WilliGomez <willgom.95@gmail.com>
2023-07-18 20:21:54 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/*function cambiarSemaforo() {
+    console.log("Rojo");
+    setTimeout(() => {
+      console.log("Verde");
+      setTimeout(() => {
+        console.log("Amarillo");
+        setTimeout(() => {
+          cambiarSemaforo(); // Reiniciar el ciclo
+        }, 2000); // Esperar 2 segundos antes de cambiar a Rojo nuevamente
+      }, 2000); // Esperar 2 segundos antes de cambiar a Amarillo
+    }, 2000); // Esperar 2 segundos antes de cambiar a Verde
+  }
+  
+  // Iniciar el ciclo del semáforo
+  cambiarSemaforo();
+*/
